acbbdd868795c5e8c32f69004d2a4077c25cbea3
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-12 14:13:11 +0000

    Chapter 02 - Exercise 01

---
+++ chapter-02/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Hello TypeScript!");
+// Exercise 1
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+//# sourceMappingURL=index.js.map
